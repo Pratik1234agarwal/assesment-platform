7d616e92f7813e3134196f799b2bc96d1e1d38e7
Pratik Agarwal <pratik1234agarwal@gmail.com>
2021-07-02 14:07:56 +0530

    Added changes

--- Nodemailer/mailTemplates/mailSendAfterTest.js
+++ Nodemailer/mailTemplates/mailSendAfterTest.js
@@ -7,25 +7,25 @@ const sendMailAfterTest = (name, email) => {
     email,
     subject:
       "Thank you for giving Advanced Data Science Aptitude Test (A-DSAT).",
-    text: `Dear ${name},
+    text: `Dear ${name},\n
 
-    Thank you giving Advanced Data Science Aptitude Test (A-DSAT).
+    Thank you giving Advanced Data Science Aptitude Test (A-DSAT).\n
     
-    Your results will be shared with you over this registered email very soon.
+    Your results will be shared with you over this registered email very soon.\n
     
-    All the best for the results.!!
+    All the best for the results.!!\n\n
     
-    Regards 
+    Regards \n
     IIT Ropar`,
-    html: `Dear ${name},
+    html: `Dear ${name},<br />
 
-    Thank you giving Advanced Data Science Aptitude Test (A-DSAT).
+    Thank you giving Advanced Data Science Aptitude Test (A-DSAT).<br />
     
-    Your results will be shared with you over this registered email very soon.
+    Your results will be shared with you over this registered email very soon.<br />
     
-    All the best for the results.!!
-    
-    Regards 
+    All the best for the results.!!<br />
+    <br/>
+    Regards <br/>
     IIT Ropar`,
   });
   sendMail(message);
