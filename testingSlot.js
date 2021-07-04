5364ebd05a2cb00e94b1ea4320d052332447c70d
Pratik Agarwal <pratik1234agarwal@gmail.com>
2021-07-04 10:58:51 +0530

    Added testingslot

--- testingSlot.js
+++ testingSlot.js
@@ -2,19 +2,46 @@ const User = require("./models/User");
 const TimeSlot = require("./models/timeSlot");
 require("./config/db")();
 
+const users = [
+  "vikramtilokani10@gmail.com",
+  "gunjan.aggarwal@theikigailab.com",
+  "sauuuchavan49@gmail.com",
+  "sahithipalla@gmail.com",
+  "tilsusis2019@gmail.com",
+  "abhay.pandey@algo8.ai",
+  "1abhay234@gmail.com",
+  "abhay2704@outlook.com",
+  "pratik.agarwal@theikigailab.com",
+];
+
 async function newSlot() {
   try {
     const timeSlot = new TimeSlot({
-      startTime: new Date(2021, 6, 3, 14, 0),
-      endTime: new Date(2021, 6, 3, 14, 30),
-      slotNumber: 48,
+      startTime: new Date(2021, 6, 4, 12, 0),
+      endTime: new Date(2021, 6, 4, 12, 30),
+      slotNumber: 100,
     });
     await timeSlot.save();
     //const user = await User.findOne({ email: "testing@test.com" });
-    await User.updateOne(
-      { email: "testing@test.com" },
-      { slotAlloted: true, timeSlot: timeSlot._id }
-    );
+
+    for (let i = 0; i < users.length; i++) {
+      const user = await User.findOne({ email: users[i] });
+      if (user) {
+        console.log("User present adding to the slot");
+        await User.updateOne(
+          { email: users[i] },
+          { slotAlloted: true, timeSlot: timeSlot._id }
+        );
+        console.log("User added to the database ", users[i]);
+      } else {
+        console.log(user[i], " Not present in database");
+      }
+    }
+
+    // await User.updateOne(
+    //   { email: "testing@test.com" },
+    //   { slotAlloted: true, timeSlot: timeSlot._id }
+    // );
   } catch (err) {
     console.log(err);
   }
