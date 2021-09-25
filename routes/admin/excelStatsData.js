54f46e293d9dc1ff729624562291bf77226714ff
Pratik Agarwal <pratik1234agarwal@gmail.com>
2021-09-25 11:50:53 +0530

    Added route

--- routes/admin/excelStatsData.js
+++ routes/admin/excelStatsData.js
@@ -10,6 +10,7 @@ const Paper = require('../../models/Paper');
 const User = require('../../models/User');
 const Event = require('../../models/Event');
 const Subtopic = require('../../models/SubTopic');
+const Test = require('../../models/Test');
 
 const users = require('../../helpers/emailArray');
 
@@ -36,6 +37,45 @@ router.get('/testNotGiven/:testId', auth, async (req, res) => {
       status: 'success',
       data: {
         data,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(serverErrorResponse());
+  }
+});
+
+router.get('/testAllDetails', auth, async (req, res) => {
+  try {
+    let data = [];
+    const tests = await Test.find().select('testName');
+    console.log(tests[0]);
+    for (let i = 0; i < users.length; i++) {
+      const user = await User.findOne({ email: users[i] });
+      if (!user) continue;
+      let temp = {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+      };
+      for (let j = 0; j < tests.length; j++) {
+        const paper = await Paper.findOne({
+          user: user._id,
+          test: tests[j]._id,
+        }).select('marks');
+        if (paper) {
+          temp[tests[j].testName] = 'Given';
+        } else {
+          temp[tests[j].testName] = 'Not Given';
+        }
+      }
+      data.push(temp);
+    }
+    return res.json({
+      status: 'success',
+      data: {
+        data,
+        length: data.length,
       },
     });
   } catch (err) {
